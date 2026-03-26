f20fa78f44d362368801568d23e3ba6d9c10bb24
Adrian Imiolek <adrian.imiolek99@gmail.com>
2026-03-26 10:03:01 +0100

    initial commit

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -2,6 +2,7 @@
 // It describes the shape of the data, and what data type each property should accept.
 // For simplicity of teaching, we're manually defining these types.
 // However, these types are generated automatically if you're using an ORM such as Prisma.
+// ORM (Object-Relational Mapping) - use Primsa or Drizzle for future typescript 
 export type User = {
   id: string;
   name: string;
